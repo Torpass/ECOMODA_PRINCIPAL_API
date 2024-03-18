8cae49e4b78fe5b41b2537af824caad02d0ce493
Francisco <fjnt15@gmail.com>
2024-03-17 22:39:31 -0400

    Añadidos mensajes de error a los validadores de Materials

--- src/validators/materialValidator.ts
+++ src/validators/materialValidator.ts
@@ -5,18 +5,18 @@ import validationResults from '../utils/handleValidations';
 export const materialValidator = [
     check('material')
         .exists()
-        .notEmpty()
-        .isString()
-        .isLength({ min: 1, max: 45 }),
+        .notEmpty().withMessage('Este campo no puede estar vacío')
+        .isString().withMessage('Debe ser una cadena de texto')
+        .isLength({ min: 1, max: 45 }).withMessage('Debe contener entre 1-45 caracteres'),
     check('unit')
         .exists()
-        .notEmpty()
-        .isString()
-        .isIn(['meters', 'unit']), // Verifica que el valor esté dentro de los permitidos ('meters' o 'unit')
+        .notEmpty().withMessage('Este campo no puede estar vacío')
+        .isString().withMessage('Debe ser una cadena de texto')
+        .isIn(['meters', 'unit']).withMessage('El valor debe estar dentro de los permitidos ("meters" o "unit")'), // Verifica que el valor esté dentro de los permitidos ('meters' o 'unit')
     check('description')
         .optional() // Es opcional ya que puede que no siempre se da una descripción
-        .isString()
-        .isLength({ max: 255 }),
+        .isString().withMessage('Debe ser una cadena de texto')
+        .isLength({ max: 255 }).withMessage('Debe contener un máximo de 255 caracteres'),
     (req: Request, res: Response, next: NextFunction) => {
         console.log(req.body)
         return validationResults(req, res, next);
